46a41c4162d2470dacbeb0e58d0b043c95f7dc4e
osmar353 <osmarm353@gmail.com>
2016-11-22 11:36:05 -0300

    clase 01

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var elNombre;
+
+	elNombre = document.getElementById("elNombre").value;
+	alert(elNombre);
+
+
+}
+
+
---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{
+	var numeroUno;
+	var numeroDos;
+	numeroUno = parseInt(document.getElementById("numeroUno"). value);
+	numeroDos = parseInt(document.getElementById("numeroDos"). value);
+	var resultado = numeroUno + numeroDos;
+	alert("la suma es"+ resultado);
+
+}
+
